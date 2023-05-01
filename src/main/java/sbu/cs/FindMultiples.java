3173db0fb62ae657e386cdbdb1277a30364f4d08
galavantmadi <galavantmadi@icloud.com>
2023-05-01 21:28:08 +0430

    commit find multiples

--- src/main/java/sbu/cs/FindMultiples.java
+++ src/main/java/sbu/cs/FindMultiples.java
@@ -19,24 +19,109 @@ package sbu.cs;
     Use the tests provided in the test folder to ensure your code works correctly.
  */
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class FindMultiples
 {
 
     // TODO create the required multithreading class/classes using your preferred method.
+    private static final int numThreads = 3;
+
+    public static class SumArray  extends Thread{
+        private int lo, hi;
+        private int[] arr;
+
+        private long ans = 0;
+
+        public SumArray(int[] arr, int lo, int hi){
+            this.lo = lo;
+            this.hi = hi;
+            this.arr = arr;
+        }
+
+        @Override
+        public void run() {
+            for (int i = lo; i < hi; i++) {
+                ans += arr[i];
+            }
+        }
+
+        public double getAns() {
+            return ans;
+        }
+    }
+
 
 
     /*
     The getSum function should be called at the start of your program.
     New Threads and tasks should be created here.
     */
-    public int getSum(int n) {
-        int sum = 0;
+    public static   long getSum(int n)  {
+        //int sum = 0;
+
 
         // TODO
+        int[] arrayNum=new int[n];
+        int[] finalArray=new int[n];
+        long ans = 0;
 
-        return sum;
+        int count=0;
+        for(int i=0;i<n;i++){
+            arrayNum[i]=i+1;
+        }
+        for (int i:arrayNum){
+            if(i%3==0){
+                if(IntStream.of(finalArray).noneMatch(x->x==i)){
+                    finalArray[count]=i;
+                    count++;
+                }
+
+            }
+        }
+        for (int i:arrayNum){
+            if(i%5==0){
+                if(IntStream.of(finalArray).noneMatch(x->x==i)){
+                    finalArray[count]=i;
+                    count++;
+                }
+
+            }
+        }
+        for (int i:arrayNum){
+            if(i%7==0){
+                if(IntStream.of(finalArray).noneMatch(x->x==i)){
+                    finalArray[count]=i;
+                    count++;
+                }
+
+            }
+        }
+
+        SumArray[] ts = new SumArray[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            ts[i] = new SumArray(finalArray, (i * finalArray.length) / numThreads, ((i + 1) * finalArray.length / numThreads));
+            ts[i].start();
+        }
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                ts[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            ans +=ts[i].getAns();
+
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
+        System.out.println("Enter the number  = ");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(getSum(num));
     }
 }
